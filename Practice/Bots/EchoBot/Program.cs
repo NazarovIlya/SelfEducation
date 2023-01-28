671a20c02a32c3190f2b9479912157c8d8c327b8
NazarovIlya <eliakohai@gmail.com>
2023-01-28 23:31:58 +0300

    simple echo bot added

---
+++ Practice/Bots/EchoBot/Program.cs
@@ -0,0 +1,16 @@
+﻿// Simplest Telegram Bot
+
+using Telegram.Bot;
+
+string token = File.ReadAllText("token.config");
+
+var client = new TelegramBotClient(token);
+
+client.StartReceiving(
+    (c, arg, _) => c.SendTextMessageAsync(
+        arg.Message.Chat.Id, "LETS FUN!!!"),
+        (_, _, _) => Task.CompletedTask);
+
+Console.WriteLine("Start echobot...");
+
+Console.ReadLine();
