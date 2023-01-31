8e10f332caf374c13e6fec204a482e2081eee71a
NazarovIlya <eliakohai@gmail.com>
2023-01-31 22:08:58 +0300

    worker class & main added

--- TheoryOfOOP/BasePrinciplesOfOOP/Inheritance/Inh001_Human/Human.cs
+++ TheoryOfOOP/BasePrinciplesOfOOP/Inheritance/Inh001_Human/Human.cs
@@ -7,16 +7,22 @@ namespace Inheritance
 {
     public class Human
     {
-        string name;
-        string lastName;
-        ushort age;
-        public Human() { }
+        protected string name;
+        protected string lastName;
+        protected ushort age;
+        // public Human() { }
+        // public Human(string name) { }
 
         public Human(string name, string lastName, ushort age)
         {
             this.name = name;
             this.lastName = lastName;
             this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}", this.name, this.lastName, this.age);
         }
     }
 }
