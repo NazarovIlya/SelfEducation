80a12ce06e52692402d163df189553d1c5e4db7a
NazarovIlya <eliakohai@gmail.com>
2023-03-23 01:10:31 +0300

    updated

--- TheoryOfOOP/DesignPatterns/NullObject_01/Cake.cs
+++ TheoryOfOOP/DesignPatterns/NullObject_01/Cake.cs
@@ -1,12 +1,20 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace NullObject_01
-//{
-//	internal class Cake : Product
-//	{
-//	}
-//}
+namespace NullObject_01
+{
+	internal class Cake : Product
+	{
+		public Cake() : base(578, "Yashkino.")
+		{
+		}
+
+		public override void eat()
+		{
+			Console.WriteLine("Sweet cakes!");
+		}
+	}
+}
--- TheoryOfOOP/DesignPatterns/NullObject_01/Program.cs
+++ TheoryOfOOP/DesignPatterns/NullObject_01/Program.cs
@@ -6,10 +6,13 @@ using NullObject_01;
 Console.Clear();
 Product milk = new Milk();
 Product coffee = new Coffee();
+Product cake = new Cake();
 
 Console.WriteLine(milk.KCalOfProduct());
 milk.eat();
 Console.WriteLine(coffee.KCalOfProduct());
 coffee.eat();
+Console.WriteLine(cake.KCalOfProduct());
+cake.eat();
 Console.ReadKey();
 
