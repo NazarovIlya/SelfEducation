e58d85551b4569e27c7a1b7d89a1b9f881e7939f
NazarovIlya <eliakohai@gmail.com>
2023-03-23 00:50:13 +0300

    null object pattern initialized

---
+++ TheoryOfOOP/DesignPatterns/NullObject_01/ListProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject_01
+{
+	internal sealed class ListProduct
+	{
+		public List<string> products;
+
+		public ListProduct()
+		{
+			this.products = new List<string>();
+			products.Add("Milk");
+			products.Add("Cakes");
+			products.Add("Cream");
+			products.Add("Soda");
+			products.Add("Cofee");
+			products.Add("Latte");
+			products.Add("Cocoa");
+			products.Add(string.Empty);
+			products.Add(string.Empty);
+		}
+	}
+}
---
+++ TheoryOfOOP/DesignPatterns/NullObject_01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Есть некий магазин с товарами
+// 
+
+using NullObject_01;
+
+Console.Clear();
+Milk milk = new();
+
+Console.WriteLine(milk.KCalOfProduct());
+milk.eat();
+Console.ReadKey();
+
